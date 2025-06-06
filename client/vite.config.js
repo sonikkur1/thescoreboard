70883705198230e174e02f524d021b1f0ed85b12
sonikkur1 <kr396@kent.ac.uk>
2025-06-06 23:54:47 +0100

    changed back to render

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://thescoreboard.onrender.com'
+      '/api': 'http://localhost:5000'
     }
   }
 });
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('https://thescoreboard.onrender.com/api/transactions', transactionsRouter);
+app.use('/api/transactions', transactionsRouter);
 
 // Connect to MongoDB and start server
 mongoose.connect(process.env.MONGODB_URI, {
