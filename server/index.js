c4d17897fb2562869b9757a4dfe8dbba61f871ab
sonikkur1 <kr396@kent.ac.uk>
2025-06-06 23:25:51 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,4 +34,4 @@ app.delete('https://thescoreboard.onrender.com/api/transactions/:id', async (req
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
