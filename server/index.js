fd5ab18b53e3776ffbeafb5d5522b2947aca2f29
sonikkur1 <kr396@kent.ac.uk>
2025-06-06 23:37:39 +0100

    _uri to db_uri

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
